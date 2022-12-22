36ce362ac6868ed3c328e1d0cae8b938129228d4
wilfred05777@gmail.com <wilfred05777@gmail..com>
2022-12-23 04:43:03 +0800

    fixing the boostrap with base_url

--- database_connection.php
+++ database_connection.php
@@ -2,7 +2,8 @@
 
 //database_connection.php
 
-$base_url = 'http://localhost/tutorial/school_fees_management_system/';
+// $base_url = 'http://localhost/tutorial/school_fees_management_system/';
+$base_url = 'http://localhost/school_fees_management_system/';
 
 $connect = new PDO("mysql:host=localhost;dbname=sfms", "root", "");
 
@@ -12,24 +13,19 @@ session_start();
 
 function is_login()
 {
-	if(isset($_SESSION['admin_id']))
-	{
-		return true;
-	}
-	return false;
+  if (isset($_SESSION['admin_id'])) {
+    return true;
+  }
+  return false;
 }
 
 function is_master_user()
 {
-	if(isset($_SESSION['user_type']))
-	{
-		if($_SESSION["user_type"] == 'Master')
-		{
-			return true;
-		}
-		return false;
-	}
-	return false;
+  if (isset($_SESSION['user_type'])) {
+    if ($_SESSION["user_type"] == 'Master') {
+      return true;
+    }
+    return false;
+  }
+  return false;
 }
-
-?>
